648be080bb27906f40bbbfbb8518982d6e2241c0
Justin Azoff <justin@bouncybouncy.net>
2009-03-29 13:34:16 -0400

    initial package creation

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+long_description = ""
+
+setup(name='netflow-indexer',
+      version=version,
+      description="Index netflow",
+      long_description=long_description,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='netflow search index',
+      author='Justin Azoff',
+      author_email='JAzoff@uamail.albany.edu',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
