e61714687a3bc75efdfe322c2d70890b017e8fe0
Justin Azoff <justin@bouncybouncy.net>
2010-04-27 09:41:16 -0400

    nicer compaction script, avoid compacting the same directory twice

--- examples/xap_compact.sh
+++ examples/xap_compact.sh
@@ -2,4 +2,9 @@
 orig="$1"
 tmp=tmp_$$.db
 tmp2=tmp2_$$.db
-quartzcompact -F $orig $tmp && mv $orig $tmp2 && mv $tmp $orig && rm -rf $tmp2
+
+if [ -e $orig/.compacted ] ; then
+    exit 0
+fi
+
+quartzcompact -F $orig $tmp && mv $orig $tmp2 && mv $tmp $orig && rm -rf $tmp2 && touch $orig/.compacted
