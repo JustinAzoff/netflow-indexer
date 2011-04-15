8ee47e0fb02198b9aa6f98b1d84f70aa2c84d067
Justin Azoff <justin@bouncybouncy.net>
2011-04-15 16:04:41 -0400

    implement netmask searching

--- netflowindexer/base/searcher.py
+++ netflowindexer/base/searcher.py
@@ -22,8 +22,37 @@ class BaseSearcher:
             yield doc
 
     def search_ips(self, ips):
-        words = [struct.pack(">L", IPy.IP(ip).int()) for ip in ips]
+        words = []
+        for ip in ips:
+            if '/' in ip:
+                words.extend(self.expand_netmask(ip))
+            else :
+                words.append(self.convert_ip(ip))
         return self.do_search(words)
+
+    def convert_ip(self, ip):
+        return struct.pack(">L", IPy.IP(ip).int())
+
+    def ips_from_network(self, network):
+        #FIXME: a /24 should do two /23 searches, not 1/16
+        net = struct.pack(">L", network.net().int())
+        strip = network.prefixlen()/8
+        prefix = net[:strip]
+
+        ips = [i.term for i in self.database.allterms(prefix)]
+        return ips
+
+    def expand_netmask(self, netmask):
+        """Expand netmask based on the terms in the database.
+           Doesn't bother using the database for tiny networks"""
+        network = IPy.IP(netmask)
+        if len(network) < 32:
+            return [struct.pack(">L", ip.int()) for ip in network]
+        ips = self.ips_from_network(network)
+
+        if network.prefixlen() not in (8,16,24):
+            ips = [ip for ip in ips if IPy.IP(struct.unpack(">L", ip)[0]) in network]
+        return ips
 
     def docid_to_date(self, fn):
         """turn /data/nfsen/profiles/live/podium/nfcapd.2009030110 into
