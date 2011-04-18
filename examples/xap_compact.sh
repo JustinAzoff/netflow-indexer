4e5cdc27dedf8eaf5fdcce75c116db5dddc9aadf
Justin Azoff <justin@bouncybouncy.net>
2011-04-18 17:40:31 -0400

    update compact command

--- examples/xap_compact.sh
+++ examples/xap_compact.sh
@@ -7,4 +7,4 @@ if [ -e $orig/.compacted ] ; then
     exit 0
 fi
 
-quartzcompact -F $orig $tmp && mv $orig $tmp2 && mv $tmp $orig && rm -rf $tmp2 && touch $orig/.compacted
+xapian-compact -F $orig $tmp && mv $orig $tmp2 && mv $tmp $orig && rm -rf $tmp2 && touch $orig/.compacted
