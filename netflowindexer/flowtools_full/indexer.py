1c1fad786f5ad62cfc30230d14f1604d4a1cae53
Justin Azoff <justin@bouncybouncy.net>
2011-04-20 15:17:31 -0400

    fix stupid bug, bump version

--- netflowindexer/flowtools_full/indexer.py
+++ netflowindexer/flowtools_full/indexer.py
@@ -4,6 +4,7 @@ class FlowToolsFullIndexer(FlowToolsIndexer):
     def fn_to_docid(self, fn):
         """turn /usr/local/var/db/flows/packeteer/2009/2009-03/2009-03-27/ft-v05.2009-03-27.161500-0400 into
                 /usr/local/var/db/flows/packeteer/2009/2009-03/2009-03-27/ft-v05.2009-03-27.1615
+        """
         
         return fn[:-7]
 
